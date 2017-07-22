62feef92b2c32aca8e9b3e0e261d8be1f9e9d58c
Roger Lee <takeru.moe@gmail.com>
2017-07-22 17:31:47 +0800

    Update keyword preset

--- src/presets/keyword.js
+++ src/presets/keyword.js
@@ -8,39 +8,57 @@
         return b.innerHTML;
     }
 
-    function filter(suggestions, needle, count) {
-        suggestions = suggestions.map(function (v) {
-            return {
-                value: String(v)
-            };
-        }).filter(function (v) {
-            var vector = [];
-            var str = v.value.toLowerCase();
-            var j = 0,
-                lastpos = -1;
-            for (var i = 0; i < needle.length; i++) {
-                var l = needle.charAt(i).toLowerCase();
-                if (l == ' ') {
-                    continue;
-                }
-                j = str.indexOf(l, j);
-                if (j == -1) {
-                    return false;
-                }
-                vector[vector.length] = j - lastpos - 1;
-                lastpos = j++;
+    function fuzzyMatch(haystack, needle) {
+        var vector = [];
+        var str = haystack.toLowerCase();
+        var j = 0;
+        var lastpos = -1;
+        for (var i = 0; i < needle.length; i++) {
+            var l = needle.charAt(i).toLowerCase();
+            if (l == ' ') {
+                continue;
             }
-            v.firstIndex = vector[0];
-            v.consecutiveMatches = /^(0+)/.test(vector.slice(0).sort().join('')) && RegExp.$1.length;
-            v.formattedText = '';
-            j = 0;
-            for (i = 0; i < vector.length; i++) {
-                v.formattedText += encode(v.value.substr(j, vector[i])) + '<b>' + encode(v.value[j + vector[i]]) + '</b>';
-                j += vector[i] + 1;
+            j = str.indexOf(l, j);
+            if (j == -1) {
+                return {
+                    firstIndex: Infinity,
+                    consecutiveMatches: -1,
+                    formattedText: haystack
+                };
             }
-            v.formattedText += encode(v.value.slice(j));
-            v.formattedText = v.formattedText.replace(/<\/b><b>/g, '');
-            return true;
+            vector[vector.length] = j - lastpos - 1;
+            lastpos = j++;
+        }
+        var firstIndex = vector[0];
+        var consecutiveMatches = /^(0+)/.test(vector.slice(0).sort().join('')) && RegExp.$1.length;
+        var formattedText = '';
+        j = 0;
+        for (i = 0; i < vector.length; i++) {
+            formattedText += encode(haystack.substr(j, vector[i])) + '<b>' + encode(haystack[j + vector[i]]) + '</b>';
+            j += vector[i] + 1;
+        }
+        formattedText += encode(haystack.slice(j));
+        return {
+            firstIndex: firstIndex,
+            consecutiveMatches: consecutiveMatches,
+            formattedText: formattedText.replace(/<\/b><b>/g, '')
+        };
+    }
+
+    function processSuggestions(suggestions, needle, count) {
+        suggestions = suggestions.filter(function (v) {
+            $.extend(v, fuzzyMatch(v.displayText, needle));
+            var m = fuzzyMatch(v.value, needle);
+            v.firstIndex = Math.min(v.firstIndex, m.firstIndex);
+            v.consecutiveMatches = Math.max(v.consecutiveMatches, m.consecutiveMatches);
+            if (v.matches) {
+                v.matches.forEach(function (w) {
+                    var m = fuzzyMatch(w, needle);
+                    v.firstIndex = Math.min(v.firstIndex, m.firstIndex);
+                    v.consecutiveMatches = Math.max(v.consecutiveMatches, m.consecutiveMatches);
+                });
+            }
+            return v.firstIndex !== Infinity;
         });
         suggestions.sort(function (a, b) {
             return ((b.consecutiveMatches - a.consecutiveMatches) + (a.firstIndex - b.firstIndex)) || a.value.localeCompare(b.value);
@@ -52,6 +70,7 @@
         options: {
             required: false,
             allowFreeInput: true,
+            allowedValues: null,
             suggestionCount: 5,
             suggestions: false,
             validate: function () {
@@ -61,22 +80,18 @@
         overrides: {
             getValue: function (preset) {
                 return $('span', this.element).map(function (i, v) {
-                    return v.childNodes[0].data;
+                    return $(v).data('value');
                 }).get();
             },
             setValue: function (preset, values) {
                 this.invoke(function (tx) {
-                    function validateAndAdd(v) {
-                        if (preset.options.validate(v)) {
-                            tx.insertWidget('tag', v);
-                        }
-                    }
+                    var add = tx.typer.invoke.bind(tx.typer, 'add');
                     tx.selection.select(tx.typer.element, 'contents');
                     tx.insertText('');
                     if ($.isArray(values)) {
-                        $.map(values, validateAndAdd);
+                        $.map(values, add);
                     } else {
-                        String(values).replace(/\S+/g, validateAndAdd);
+                        String(values).replace(/\S+/g, add);
                     }
                 });
             },
@@ -97,7 +112,7 @@
                 inline: true,
                 editable: 'none',
                 insert: function (tx, value) {
-                    tx.insertHtml('<span class="typer-ui-keyword">' + value + '<i>delete</i></span>');
+                    tx.insertHtml('<span class="typer-ui-keyword" data-value="' + encode(value.value) + '">' + encode(value.displayText) + '<i>delete</i></span>');
                 },
                 click: function (e) {
                     if (e.target !== e.widget.element) {
@@ -108,9 +123,18 @@
         },
         commands: {
             add: function (tx, value) {
-                if (!value || tx.typer.getValue().indexOf(value) >= 0) {
+                if (!value || tx.typer.getValue().indexOf(value.value || value) >= 0) {
                     return;
                 }
+                if (typeof value === 'string') {
+                    value = {
+                        value: value,
+                        displayText: value
+                    };
+                }
+                // TODO: map display text from previous suggestions
+                // and validate from suggestions
+
                 var lastSpan = $('span:last', tx.typer.element)[0];
                 if (lastSpan) {
                     tx.selection.select(lastSpan, false);
@@ -123,25 +147,31 @@
                 tx.insertText('');
 
                 var preset = tx.typer.getStaticWidget('__preset__');
-                $(lastSpan).toggleClass('invalid', (!preset.options.allowFreeInput && preset.allowedValues.indexOf(value) < 0));
+                if ((!preset.options.allowFreeInput && preset.allowedValues.indexOf(value.value) < 0) || !preset.options.validate(value.value)) {
+                    $(lastSpan).addClass('invalid');
+                }
             }
         },
         init: function (e) {
             e.typer.getSelection().moveToText(e.typer.element, -0);
-            e.widget.callout = $('<div class="typer-ui typer-ui-float"><div class="typer-ui-buttonlist"></div></div>')[0];
-            $(e.widget.callout).on('click', 'button', function (e2) {
-                e.typer.invoke('add', $(e2.currentTarget).text());
-                $(e.typer.element).focus();
-                $(e.widget.callout).detach();
+            e.widget.allowedValues = [];
+            e.widget.suggestions = [];
+            e.widget.callout = Typer.ui({ 
+                type: 'contextmenu',
+                typer: e.typer
             });
-            e.typer.retainFocus(e.widget.callout);
+            $(e.widget.callout.element).on('click', 'button', function (e2) {
+                e.typer.invoke('add', e.widget.suggestions[$(this).index()]);
+                e.typer.getSelection().focus();
+                e.widget.callout.hide();
+            });
         },
         click: function (e) {
             e.widget.selectedIndex = -1;
         },
         focusout: function (e) {
             e.widget.selectedIndex = -1;
-            $(e.widget.callout).detach();
+            e.widget.callout.hide();
             var value = Typer.trim(e.typer.extractText());
             if (value && e.widget.options.validate(value)) {
                 e.typer.invoke('add', value);
@@ -149,17 +179,17 @@
         },
         upArrow: function (e) {
             if (e.widget.selectedIndex >= 0) {
-                $('button', e.widget.callout).removeClass('active').eq(e.widget.selectedIndex--).prev().addClass('active');
+                $('button', e.widget.callout.element).removeClass('active').eq(e.widget.selectedIndex--).prev().addClass('active');
             }
         },
         downArrow: function (e) {
             if (e.widget.selectedIndex < $('button', e.widget.callout).length - 1) {
-                $('button', e.widget.callout).removeClass('active').eq(++e.widget.selectedIndex).addClass('active');
+                $('button', e.widget.callout.element).removeClass('active').eq(++e.widget.selectedIndex).addClass('active');
             }
         },
         enter: function (e) {
             if (e.widget.selectedIndex >= 0) {
-                e.typer.invoke('add', $('button', e.widget.callout).eq(e.widget.selectedIndex).text());
+                e.typer.invoke('add', e.widget.suggestions[e.widget.selectedIndex]);
                 e.widget.selectedIndex = -1;
             } else {
                 var value = Typer.trim(e.typer.extractText());
@@ -171,29 +201,47 @@
         keystroke: function (e) {
             if (e.data === 'escape' && $.contains(document.body, e.widget.callout)) {
                 e.widget.selectedIndex = -1;
-                $(e.widget.callout).detach();
+                e.widget.callout.hide();
                 e.preventDefault();
             }
         },
         contentChange: function (e) {
             if (e.data === 'textInput' || e.data === 'keystroke') {
                 var value = Typer.trim(e.typer.extractText());
-                var suggestions = e.widget.options.suggestions || [];
+                var suggestions = e.widget.options.suggestions || e.widget.options.allowedValues || [];
                 if ($.isFunction(suggestions)) {
                     suggestions = suggestions(value);
                 }
                 $.when(suggestions).done(function (suggestions) {
-                    var currentValues = e.typer.getValue();
-                    e.widget.allowedValues = suggestions;
-                    suggestions = suggestions.filter(function (v) {
-                        return currentValues.indexOf(v) < 0;
+                    suggestions = suggestions.map(function (v) {
+                        if (typeof v === 'string') {
+                            return {
+                                value: v,
+                                displayText: v
+                            };
+                        }
+                        return v;
                     });
-                    suggestions = filter(suggestions, value, e.widget.options.suggestionCount);
+                    suggestions.forEach(function (v) {
+                        if (e.widget.allowedValues.indexOf(v.value) <= 0) {
+                            e.widget.allowedValues.push(v.value);
+                        }
+                    });
+
+                    var currentValues = e.typer.getValue();
+                    suggestions = suggestions.filter(function (v) {
+                        return currentValues.indexOf(v.value) < 0;
+                    });
+                    suggestions = processSuggestions(suggestions, value, e.widget.options.suggestionCount);
                     if (value && e.widget.options.allowFreeInput) {
                         suggestions.push({
+                            value: value,
+                            displayText: value,
                             formattedText: '<i>' + encode(value) + '</i>'
                         });
                     }
+                    e.widget.suggestions = suggestions;
+
                     var html;
                     if (suggestions.length) {
                         html = '<button>' + suggestions.map(function (v) {
@@ -202,17 +250,11 @@
                     } else {
                         html = '<button class="disabled">No suggestions</button>';
                     }
-                    $(e.widget.callout).children().html(html);
+                    $('.typer-ui-buttonlist', e.widget.callout.element).html(html);
                 });
                 setTimeout(function () {
-                    if (e.typer.focused() && !$.contains(document.body, e.widget.callout)) {
-                        var r = e.typer.element.getBoundingClientRect();
-                        $(e.widget.callout).appendTo(document.body).css({
-                            position: 'fixed',
-                            top: r.bottom,
-                            left: r.left
-                        });
-                        Typer.ui.setZIndex(e.widget.callout, e.typer.element);
+                    if (e.typer.focused()) {
+                        e.widget.callout.show(e.typer.element);
                     }
                 });
             }
