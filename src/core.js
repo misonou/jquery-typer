543f0e862d8f3b0b840c3c5f4c4d90f103b14f91
Takeru Lee <takeru.moe@gmail.com>
2017-12-15 22:14:40 +0800

    Handle single touch as click

--- src/core.js
+++ src/core.js
@@ -4,7 +4,7 @@
     var KEYNAMES = JSON.parse('{"8":"backspace","9":"tab","13":"enter","16":"shift","17":"ctrl","18":"alt","19":"pause","20":"capsLock","27":"escape","32":"space","33":"pageUp","34":"pageDown","35":"end","36":"home","37":"leftArrow","38":"upArrow","39":"rightArrow","40":"downArrow","45":"insert","46":"delete","48":"0","49":"1","50":"2","51":"3","52":"4","53":"5","54":"6","55":"7","56":"8","57":"9","65":"a","66":"b","67":"c","68":"d","69":"e","70":"f","71":"g","72":"h","73":"i","74":"j","75":"k","76":"l","77":"m","78":"n","79":"o","80":"p","81":"q","82":"r","83":"s","84":"t","85":"u","86":"v","87":"w","88":"x","89":"y","90":"z","91":"leftWindow","92":"rightWindowKey","93":"select","96":"numpad0","97":"numpad1","98":"numpad2","99":"numpad3","100":"numpad4","101":"numpad5","102":"numpad6","103":"numpad7","104":"numpad8","105":"numpad9","106":"multiply","107":"add","109":"subtract","110":"decimalPoint","111":"divide","112":"f1","113":"f2","114":"f3","115":"f4","116":"f5","117":"f6","118":"f7","119":"f8","120":"f9","121":"f10","122":"f11","123":"f12","144":"numLock","145":"scrollLock","186":"semiColon","187":"equalSign","188":"comma","189":"dash","190":"period","191":"forwardSlash","192":"backtick","219":"openBracket","220":"backSlash","221":"closeBracket","222":"singleQuote"}');
     var VOID_TAGS = 'area base br col command embed hr img input keygen link meta param source track wbr'.split(' ');
     var INNER_PTAG = 'h1,h2,h3,h4,h5,h6,p,q,blockquote,pre,code,li,caption,figcaption,summary,dt,th';
-    var SOURCE_PRIORITY = 'keystroke textInput mouse cut paste'.split(' ');
+    var SOURCE_PRIORITY = 'keystroke textInput mouse touch cut paste'.split(' ');
     var ZWSP = '\u200b';
     var ZWSP_ENTITIY = '&#8203;';
     var EMPTY_LINE = '<p>&#8203;</p>';
@@ -1275,18 +1275,28 @@
             var modifiedKeyCode;
             var keyDefaultPrevented;
             var hasKeyEvent;
+            var touchObj;
             var activeWidget;
 
             function getEventName(e, suffix) {
                 return lowfirst(((e.ctrlKey || e.metaKey) ? 'Ctrl' : '') + (e.altKey ? 'Alt' : '') + (e.shiftKey ? 'Shift' : '') + capfirst(suffix));
             }
 
-            function triggerWidgetFocusout() {
+            function triggerWidgetFocusout(source) {
                 var widget = activeWidget;
                 if (activeWidget && !activeWidget.destroyed) {
                     activeWidget = null;
-                    triggerEvent(null, widget, 'focusout');
+                    triggerEvent(source, widget, 'focusout');
                 }
+            }
+
+            function triggerClick(source, e, point) {
+                var props = {
+                    clientX: (point || e).clientX,
+                    clientY: (point || e).clientY,
+                    target: e.target
+                };
+                triggerEvent(source, EVENT_HANDLER, getEventName(e, 'click'), null, props);
             }
 
             function updateFromNativeInput() {
@@ -1294,7 +1304,7 @@
                 if (!userFocus.has(typer) && activeRange && !rangeEquals(activeRange, createRange(currentSelection))) {
                     currentSelection.select(activeRange);
                     if (currentSelection.focusNode.widget !== activeWidget) {
-                        triggerWidgetFocusout();
+                        triggerWidgetFocusout('textInput');
                     }
                     var timeStamp = +new Date();
                     setImmediate(function () {
@@ -1500,7 +1510,7 @@
             $self.bind('mousedown mouseup mscontrolselect', function (e) {
                 var node = typer.getNode(e.target);
                 if (activeWidget !== node.widget) {
-                    triggerWidgetFocusout();
+                    triggerWidgetFocusout('mouse');
                 }
                 if (is(node, NODE_WIDGET | NODE_INLINE_WIDGET)) {
                     // disable resize handle on image element on IE and Firefox
@@ -1511,9 +1521,20 @@
                     currentSelection.select(node.widget.element);
                     if (activeWidget !== node.widget) {
                         activeWidget = node.widget;
-                        triggerEvent(null, node.widget, 'focusin');
+                        triggerEvent('mouse', node.widget, 'focusin');
                     }
                 }
+            });
+
+            $self.bind('touchstart touchmove touchend', function (e) {
+                if (e.type === 'touchend' && touchObj) {
+                    currentSelection.moveToPoint(touchObj.clientX, touchObj.clientY);
+                    currentSelection.focus();
+                    triggerClick('touch', e, touchObj);
+                    e.preventDefault();
+                }
+                var touches = e.originalEvent.touches;
+                touchObj = e.type === 'touchstart' && !touches[1] && touches[0];
             });
 
             $self.bind('contextmenu', function (e) {
@@ -1524,12 +1545,7 @@
             });
 
             $self.bind('click', function (e) {
-                var props = {
-                    clientX: e.clientX,
-                    clientY: e.clientY,
-                    target: e.target
-                };
-                triggerEvent('mouse', EVENT_HANDLER, getEventName(e, 'click'), null, props);
+                triggerClick('mouse', e);
                 e.preventDefault();
             });
 
